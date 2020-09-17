51ae7184c640bee3a5c4360ec4aa10b3a57cd2bc
Thraac <nathanofwise@gmail.com>
2020-09-17 06:42:02 -0600

    09/16/2020 - using json now and def

--- AshOrSnow/city-lookup.py
+++ AshOrSnow/city-lookup.py
@@ -1,38 +1,30 @@
 from django.http import HttpResponse
 import requests
 import json
-# import testContent
 
 # constants
 api_key = 'LXGGpNcbto33jbyFGv0yBADrLm8KEu8y'
-
-# need to take input
-# search = input("Please enter the city you want to").lower()
 search = 'denver' # for test
 city_response = '347810' # for test
-# need input to be searched with city finder API
 
-#the two city constants need to be unfrozen
-# city_url = (f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={search}") # location search
-# city_response = requests.get(city_url).json()[0]["Key"] # location search response with just the city ID
+def weatherLookup():
+    # need to take input
+    # search = input("Please enter the city you want to search: ").lower()
 
-# need to take results and find just the city number
-# need to take city number and search weather api with it
+    # need input to be searched with city finder API
+    city_url = (f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={search}") # location search
 
-# unfreeze the three below when done testing
-weather_url = (f"http://dataservice.accuweather.com/currentconditions/v1/{city_response}?apikey={api_key}")
-weather_response = requests.get(weather_url)
-# print(weather_response.content)
-temperature = weather_response.json()[0]['Temperature']['Imperial']['Value']
-weather_type = weather_response.json()[0]['WeatherText']
+    # need to take results and find just the city number
+    city_response = requests.get(city_url).json()[0]["Key"] # location search response with just the city ID
+
+    # need to take city number and search weather api with it
+    weather_url = (f"http://dataservice.accuweather.com/currentconditions/v1/{city_response}?apikey={api_key}") # uses the city response and the api key to grab weather information
+    weather_response = requests.get(weather_url)
+
+    # need to return information for weather for that city
+    temperature = weather_response.json()[0]['Temperature']['Imperial']['Value'] # this is the temperature
+    weather_type = weather_response.json()[0]['WeatherText'] # this is the type of weather right now
+    weather_icon_number = weather_response.json()[0]['WeatherIcon'] # this is an int32 image that displays accuweathers image for the current weather state
+    weather_icon = (f"https://developer.accuweather.com/sites/default/files/{weather_icon_number}-s.png")
 
 
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-jprint(weather_response.json())
-print(temperature)
-print(weather_type)
-# need to return information for weather for that city
