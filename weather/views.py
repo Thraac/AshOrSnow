4dd6c56d61148b7f3ad70a76dcf554e9083a4b0d
Thraac <nathanofwise@gmail.com>
2020-09-29 15:44:52 -0600

    09/29/2020 - better comments

--- weather/views.py
+++ weather/views.py
@@ -8,14 +8,18 @@ from .models import CityLookup, Weather, Temperature, WeatherIcon
 
 
 def home(request):
-    city_list = CityLookup.objects.all()
-    context = {'city_list': city_list}
+    """
+    View for the base home page
+    """
     return render(request, 'home.html', context)
 
 
 
 def results(request, city_id):
-
+    """
+    View for displaying weather information for searched city
+    Pulls all information from the city modules related to the city
+    """
     city = get_object_or_404(CityLookup, pk=city_id)
     weather = get_object_or_404(Weather, pk=city_id)
     temperature = get_object_or_404(Temperature, pk=city_id)
@@ -29,41 +33,45 @@ def results(request, city_id):
                 'temperature': temperature, 'weather_icon': weather_icon})
 
 
-# if you are trying to use this code and it does not work you might need your own api key (limited to 50 searches a day)
+
 def search(request, *args, **kwargs):
+    """
+    If you are trying to use this code and it does not work you might need your own api key (limited to 50 searches a day)
+    This grabs the information input in the search bar and grabs it from the Accuweather Api
+    If the search was not spelled correctly or the code limit has been reached it will display the error in except
+    Everything under else is taking the data from acuweather and asigning it to the proper module
+    Finally the view pulls up the ID of the searched city and redirects to display the results
+    """
+    
     api_key = 'LXGGpNcbto33jbyFGv0yBADrLm8KEu8y' 
 
     try:
         # grabs the city that was input 
         input_city = request.POST.get('city')
         city_url = (f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={input_city}") 
-        city_response = requests.get(city_url).json()[0]["Key"] # saves the response from api for city ID
+        city_response = requests.get(city_url).json()[0]["Key"]
 
     except:
-        # Set up the search again and lets them know they did not have a search
-        #  (KeyError, CityLookup.DoesNotExist)
+        # Sets up the search again if the code errors for any reason
         return render(request, 'home.html', {
             'error_message': "The city you submitted was not found. Please try again.",
         })
 
     else:
         # this section of code is where the look up of weather information occurs
+        weather_url = (f"http://dataservice.accuweather.com/currentconditions/v1/{city_response}?apikey={api_key}") 
+        weather_response = requests.get(weather_url) 
+        weather_icon_number = int(weather_response.json()[0]['WeatherIcon']) 
 
-        weather_url = (f"http://dataservice.accuweather.com/currentconditions/v1/{city_response}?apikey={api_key}") # uses the city response and the api key to grab weather information
-        weather_response = requests.get(weather_url) # saves the response from api for weather details
-        weather_icon_number = int(weather_response.json()[0]['WeatherIcon']) # saves the number for the weather icon and converts it to int for following code
-        if weather_icon_number<10: # this makes sure that if the icon number is below 10 it will change the number
-            weather_icon_number = str(weather_icon_number).zfill(2) # this adds a zero infront of the icon number because it needs to be 01 no 1 or it will fail
+        # the icon url requires 01 and api returns 1 this adds a 0 before the number if less than 10
+        if weather_icon_number<10: 
+            weather_icon_number = str(weather_icon_number).zfill(2) 
             
-        temperature = weather_response.json()[0]['Temperature']['Imperial']['Value'] # this is the temperature
-        weather_type = weather_response.json()[0]['WeatherText'] # this is the type of weather right now
+        temperature = weather_response.json()[0]['Temperature']['Imperial']['Value']
+        weather_type = weather_response.json()[0]['WeatherText'] 
+        weather_icon = (f"https://developer.accuweather.com/sites/default/files/{weather_icon_number}-s.png") 
 
-
-        weather_icon = (f"https://developer.accuweather.com/sites/default/files/{weather_icon_number}-s.png") # saves the url for the weather icon
-        # weather look up ends here
-
-        # This is where data is being saved to the database for each module
-        # saving the input to the city
+        # Saving searches to database
         new_city = CityLookup(city = input_city)
         new_city.save()
 
@@ -77,6 +85,6 @@ def search(request, *args, **kwargs):
         new_weather_icon.save()
 
         # Redirects to the results page since data is being posted
-        test = new_city.id
-        return HttpResponseRedirect(reverse('weather:results', args=(test,)))
+        new_city_id = new_city.id
+        return HttpResponseRedirect(reverse('weather:results', args=(new_city_id,)))
 
